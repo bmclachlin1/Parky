2a195a169c8b078ab26b1f9a489ce41748d81c4b
Blake <blake@alertlabs.com>
2023-05-27 16:01:07 -0600

    First commit

---
+++ lib/components/auth_gate.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'landing_page.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return SignInScreen(
+            providerConfigs: [
+              const EmailProviderConfiguration(),
+              GoogleProviderConfiguration(
+                  clientId: dotenv.env["googleProviderClientId"]!)
+            ],
+            subtitleBuilder: (context, action) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: action == AuthAction.signIn
+                    ? const Text(
+                        'Welcome to Vehicle.io, please sign in to register your vehicle!')
+                    : const Text(
+                        'Welcome to Vehicle.io, please sign up to register your vehicle!'),
+              );
+            },
+          );
+        }
+        return const LandingPage();
+      },
+    );
+  }
+}
